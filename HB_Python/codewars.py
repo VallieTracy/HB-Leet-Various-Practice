13a55a5ff58c7bacaa8c30cc4e36a513af0fc53c
Vallie Tracy <55700939+VallieTracy@users.noreply.github.com>
2020-12-28 18:57:55 -0600

    working on repeat letters for codewars

--- HB_Python/codewars.py
+++ HB_Python/codewars.py
@@ -1,64 +1,15 @@
-# password = "lakshdf  a;lsk a;oi q2390847..??/1"
+my_str = 'isIsogram'
 
-# def maskify(str):
-#     lst = list(str)
-#     if len(lst) > 4:
-#         last_four = lst[len(lst)-4:]
-#         for i in range(0, len(lst)-4):
-#             lst[i] = "#"
-#         output = ''.join(lst) 
-#         return output
-#     else:
-#         return str       
-
-
-# print(maskify(password))
-
-# def get_count(input_str):
-#     num_vowels = 0
-#     vowels = 'aeiou'
-#     for i in range(len(input_str)):
-#         if input_str[i] in vowels:
-#             num_vowels += 1
+def is_isogram(string):
     
-#     return num_vowels
-
-# print(get_count("hi vallie ee"))
-
-# nums = [1,2,6,7,3,6,8,7,1]
-
-# def sum67(numbers):
-#     while 6 in numbers:
-#         idx6 = numbers.index(6)
-#         idx7 = numbers.index(7)
-#         del(numbers[idx6:idx7+1])
-#     return sum(numbers)
-
-# total = sum67(nums)
-# print(f"TOTAL SUM: {total}")
-
-my_list = [2, 2, 2]
-
-# def has22(arr):
-#     for i in range(len(arr)-1):
-#         if arr[i] == 2 and arr[i+1] == 2:
-#             return True
-#         else:
-#             return False
-
-def has22(arr):
-    while False:
-        for i in range(len(arr)-1):
-            if arr[i] == 2 and arr[i+1] == 2:
-                print(arr[i])
-                print(arr[i+1])
-                
-        else:
+    for i in range(len(string)):
+        
+        if string[i] in string[i:]:
             return False
-       
-answer = has22(my_list)
-print(f"ANSWER = {answer}")
+        else:
+            return True
 
+print(is_isogram(my_str))
 
 
 
--- HB_Python/lists_2.py
+++ HB_Python/lists_2.py
@@ -36,7 +36,7 @@
 
 # print(centered_average(my_nums))
 
-my_nums = [1, 2, 13, 4, 6, 6, 13, 13]
+
 
 # def sum13(nums):
 #     count = 0
@@ -64,19 +64,52 @@ my_nums = [1, 2, 13, 4, 6, 6, 13, 13]
 
     # return count
 
-def sum13(nums):
-    count = 0
-    i = 0
-    while i <= len(nums):
-        if nums[i] in nums[-2:]:
-            count = count + nums[i]
-            i = i + 1
-            print(f"COUNT: {count}")
-            print(f"i = i + 1: {i}")
-            print("-----------------")
+# my_nums = [1, 2, 13, 4, 13, 2, 3] #length = 5, i in 0, 4
+
+# def sum13(nums):
+#     count = 0
+#     for i in range(len(nums)):
+#         if nums[i] != 13:
+#             count += nums[i] 
+#         elif i < len(nums) - 1:
+#             if nums[i+1] != 13:
+#                 count -= nums[i+1]
+
+#     return count     
+
+# print(sum13(my_nums))
+
+# my_nums = [1, 6, 7, 1, 6, 7, 2, 6, 1, 3, 5, 7, 14]
+ 
+# def sum67(arr):
+#     while 6 in arr: 
+#         idx6 = arr.index(6)
+#         idx7 = arr.index(7)
+#         print(f"idx7: {idx7}")
+#         del arr[idx6:idx7+1] 
+    
+#     return sum(arr)
         
 
-print(sum13(my_nums))
+# print(sum67(my_nums))
+
+my_nums = [1, 2, 2, 4]
+
+def has22(nums):
+    while 2 in nums:
+        for i in range(len(nums)-1):
+            if nums[i] == 2:
+                if nums[i+1] == 2:
+                    return True
+    else:
+        return False
+
+print(has22(my_nums))
+
+
+        
+
+
 
 
 
