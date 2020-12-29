f4162a40803444490169652d4899dc584a782d51
Vallie Tracy <55700939+VallieTracy@users.noreply.github.com>
2020-12-29 06:54:54 -0600

    finished isogram exercise, next is Persistent Bugger

--- HB_Python/codewars.py
+++ HB_Python/codewars.py
@@ -1,15 +1,21 @@
-my_str = 'isIsogram'
+# my_str = 'moOse'
 
-def is_isogram(string):
-    
-    for i in range(len(string)):
-        
-        if string[i] in string[i:]:
-            return False
-        else:
-            return True
+# def is_isogram(string):
+#     string = string.lower()
+#     for letter in string:
+#         if string.count(letter) > 1:
+#             return False
+#     return True
 
-print(is_isogram(my_str))
+# result = is_isogram(my_str)
+# print(result)
+
+
+
+
+
+
+
 
 
 
