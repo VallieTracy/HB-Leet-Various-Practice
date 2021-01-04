bfe6d96e5bb7c8fd96c1ccfef55a5713947c187c
Vallie Tracy <55700939+VallieTracy@users.noreply.github.com>
2021-01-04 06:37:13 -0600

    working on counting sheep

--- HB_Python/codewars.py
+++ HB_Python/codewars.py
@@ -10,9 +10,53 @@
 # result = is_isogram(my_str)
 # print(result)
 
-number = 34
+# my_str = 'kkkwwwaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz'
 
-print(number[0])
+# def printer_error(s):
+#     ok = 'abcdefghijklm'
+#     count = 0
+#     for i in range(len(s)):
+#         if s[i] in ok:
+#             count +=1
+#     d = len(s)
+#     errors = d - count
+#     return f"{errors}/{d}"
+
+# print(printer_error(my_str))
+
+# my_string = 'xdvFFgoo'
+
+# def xo(s):
+#     s = s.lower()
+#     return s.count('x') == s.count('o')
+
+# print(xo(my_string))
+
+# def quarter_of(month):
+#     q1 = range(1, 4)
+#     q2 = range(4, 7)
+#     q3 = range(7, 10)
+#     q4 = range(10, 13)
+#     if month in q1:
+#         return 1
+#     if month in q2:
+#         return 2
+#     if month in q3:
+#         return 3
+#     if month in q4:
+#         return 4
+ 
+# print(quarter_of(13))
+
+def count_sheep(n):
+    repeat = " sheep..."
+    return f"{n}{repeat}{n-1}{repeat}{n-2}{repeat}"
+
+print(count_sheep(5))
+
+
+
+
 
 
 
